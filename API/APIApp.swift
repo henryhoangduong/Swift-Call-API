8f821b9ae466ac08d2dbb6021e1ed98a536dd962
henryhoangduong <henryhoangduong@gmail.com>
2024-12-12 10:11:56 +0700

    Initial Commit

---
+++ API/APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIApp.swift
+//  API
+//
+//  Created by mac on 12/12/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APITests/APITests.swift
@@ -0,0 +1,17 @@
+//
+//  APITests.swift
+//  APITests
+//
+//  Created by mac on 12/12/2567 BE.
+//
+
+import Testing
+@testable import API
+
+struct APITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
